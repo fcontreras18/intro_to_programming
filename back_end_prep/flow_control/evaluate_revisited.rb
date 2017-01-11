96e29ac8855b778f4b6a64374e882bbd5af77c9c
Francisco Echeverria <fcontreras18@gmail.com>
2017-01-11 14:07:39 -0800

    Add final exercise

---
+++ back_end_prep/flow_control/evaluate_revisited.rb
@@ -0,0 +1,47 @@
+def evaluate_num(number)
+  if number < 0
+    puts "You can't enter a negative number"
+  elsif number <= 50
+    puts "#{number} is between 0 and 50"
+  elsif number <= 100
+    puts "#{number} is between 51 and 100"
+  else
+    puts "#{number} is above 100"
+  end
+end
+
+def evaluate_case1_num(number)
+  case 
+  when number < 0
+    puts "You can't enter a negative number"
+  when number <= 50
+    puts "#{number} is between 0 and 50"
+  when number <= 100
+    puts "#{number} is between 51 and 100"
+  else
+    puts "#{number} is above 100"
+  end
+end
+  
+def evaluate_case2_num(number)
+  case number
+  when 0..50
+    puts "#{number} is between 0 and 50"
+  when 51..100
+    puts "#{number} is between 51 and 100"
+  else
+    if number < 0
+      puts "You can't enter a negative number"
+    else
+      puts "#{number} is above 100"
+    end
+  end
+end
+
+puts "Please enter a number between 0 and 100."
+number = gets.chomp.to_i
+
+evaluate_num(number)
+evaluate_case1_num(number)
+evaluate_case2_num(number)
+  
